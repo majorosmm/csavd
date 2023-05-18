949db3737ec71f7d7649e51f41875e72cfd9a767
majorosmm <112930769+majorosmm@users.noreply.github.com>
2023-05-18 14:53:05 +0200

    Add files via upload

---
+++ webshopcsavarz/js/custom.js
@@ -0,0 +1,9 @@
+
+
+// nav menu
+function openNav() {
+    document.getElementById("myNav").classList.toggle("menu_width");
+    document
+        .querySelector(".custom_menu-btn")
+        .classList.toggle("menu_btn-style");
+}
